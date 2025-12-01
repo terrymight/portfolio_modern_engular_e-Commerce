94a38f9773b587003b519dcdc6c69ffbfc3b9f09
Tejiri Mayone <terrymayone@gmail.com>
2025-12-01 20:58:57 +0100

    [feature] created the login component

--- src/app/features/features.routes.ts
+++ src/app/features/features.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const productRoutes: Routes = [
     {
+        path: 'login',
+        loadComponent: () => import('./login/login.component')
+        .then(m => m.LoginComponent)
+    },
+    {
         path: '',
         loadComponent: () => import('./products/products')
         .then(m => m.Products )
